2a4295cf9b35c3e485e3e53cc0314fe16828a536
vincentmugondora <vinmugondora@gmail.com>
2025-04-26 16:52:42 +0200

    creating variables

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-echo "hello vincent"
+
+define('NAME', 'Vincent');
+
+// $name = 'Vincent Mugondora';
+$age = 30;
+
+
+// $name = "Mugo";
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorials</title>
+</head>
+<body>
+
+    <h1>User Profile</h1>
+
+    <div><?php echo NAME ?></div>
+    <div><?php echo $age ?></div>
+
+</body>
+</html>
