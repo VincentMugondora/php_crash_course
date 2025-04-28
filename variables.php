30baa5e5aff6905ab09fa3c3e47cb7194ea10f6a
vincentmugondora <vinmugondora@gmail.com>
2025-04-28 13:57:57 +0200

    add variables and basic mathematical operations

---
+++ variables.php
@@ -0,0 +1,37 @@
+<?php 
+
+// integer
+$radius = 25;
+
+// float
+$pi = 3.14;
+
+// basic mathematical operations
+// echo $radius * $pi; // multiplication
+// echo $radius + $pi; // addition
+// echo $radius - $pi; // subtraction
+// echo $radius / $pi; // division
+// echo $radius % $pi; // modulus
+// echo $radius ** $pi; // exponentiation
+
+// echo $radius++; // incrementing
+// echo $radius--; // decrementing
+
+$age = 20;
+$age += 5; // incrementing by 5
+$age -= 5; // decrementing by 5
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorials</title>
+</head>
+<body>
+    
+</body>
+</html>
