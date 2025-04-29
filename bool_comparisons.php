b142cf3aa24da3559065304bb49038b939d66a63
vincentmugondora <vinmugondora@gmail.com>
2025-04-29 15:05:01 +0200

    fix: update comments in bool_comparisons.php to use proper syntax

--- bool_comparisons.php
+++ bool_comparisons.php
@@ -1,4 +1,4 @@
-<!-- Comparisons booleans (true or false) --> 
+// Comparisons booleans (true or false) 
 echo true;
 
-<!-- loose vs strict equal comparison -->
+// loose vs strict equal comparison
